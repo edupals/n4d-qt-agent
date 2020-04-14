5a9a6ec4a8dda5be0897dd61ddb6e58696f968dc
Lt-Henry <quiqueiii@gmail.com>
2020-04-14 18:02:25 +0200

    Added c++ agent wrapper (not working yet)

---
+++ lib/agent.cpp
@@ -0,0 +1,72 @@
+/*
+    N4D Qt agent
+
+    Copyright (C) 2020  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "agent.hpp"
+
+#include <unistd.h>
+#include <iostream>
+
+using namespace edupals::n4d;
+using namespace std;
+
+Agent::Agent()
+{
+}
+
+void Agent::run()
+{
+    char* args[]={"/usr/bin/n4d-qt-agent",nullptr};
+    
+    pipe(rpipe);
+    
+    pid_t pid = fork();
+    
+    if (pid==-1) {
+        throw exception::SpawnError();
+    }
+    
+    if (pid==0) {
+        
+        close(rpipe[0]);
+        
+        dup2(rpipe[1],1); //stdout
+        
+        close(rpipe[1]);
+        
+        execv("/usr/bin/n4d-qt-agent",args);
+        exit(0);
+    }
+    else {
+        close(rpipe[1]);
+        child = system::Process(pid);
+    }
+}
+
+bool Agent::is_running()
+{
+    return child.exists();
+}
+
+string Agent::get_ticket()
+{
+    char buffer[1024];
+    read(rpipe[0],buffer,1024);
+    
+    return string(buffer);
+}
---
+++ lib/agent.hpp
@@ -0,0 +1,64 @@
+/*
+    N4D Qt agent
+
+    Copyright (C) 2020  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef N4D_AGENT_LIB
+#define N4D_AGENT_LIB
+
+#include <process.hpp>
+
+#include <string>
+#include <exception>
+
+namespace edupals
+{
+    namespace n4d
+    {
+        namespace exception
+        {
+            class SpawnError : public std::exception
+            {
+                public:
+                
+                const char* what() const throw()
+                {
+                    return "Failed to spawn N4D Qt agent";
+                }
+            };
+        }
+        
+        class Agent
+        {
+            protected:
+            
+            system::Process child;
+            int rpipe [2];
+            
+            public:
+            
+                
+            Agent();
+            
+            void run();
+            bool is_running();
+            std::string get_ticket();
+        };
+    }
+}
+
+#endif
